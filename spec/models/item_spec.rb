67512043940876f71a24df220fec1e72be831595
hoshiteru0131 <nunotokoro@gmail.com>
2024-08-12 15:46:29 +0900

    商品出品機能修正

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -42,6 +42,52 @@ RSpec.describe Item, type: :model do
       expect(@item.errors.full_messages).to include('Price is not a number')
     end
 
-    # その他のバリデーションに関するテストも同様に書きます
+    it '商品画像が空では出品できない' do
+      @item.image = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Image can't be blank")
+    end
+
+    it '商品説明が空では出品できない' do
+      @item.description = ''
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Description can't be blank")
+    end
+
+    it 'カテゴリーに「---」が選択されている場合は出品できない' do
+      @item.category_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Category can't be blank")
+    end
+
+    it '商品の状態に「---」が選択されている場合は出品できない' do
+      @item.condition_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Condition can't be blank")
+    end
+
+    it '配送料の負担に「---」が選択されている場合は出品できない' do
+      @item.shipping_fee_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
+    end
+
+    it '発送元の地域に「---」が選択されている場合は出品できない' do
+      @item.prefecture_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+    end
+
+    it '発送までの日数に「---」が選択されている場合は出品できない' do
+      @item.shipping_day_id = 1
+      @item.valid?
+      expect(@item.errors.full_messages).to include("Shipping day can't be blank")
+    end
+
+    it 'userが紐付いていなければ出品できない' do
+      @item.user = nil
+      @item.valid?
+      expect(@item.errors.full_messages).to include('User must exist')
+    end
   end
 end
