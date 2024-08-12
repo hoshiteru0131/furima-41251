5b6fbd3a5647305977c1c7bb69bdee11800d93a7
hoshiteru0131 <nunotokoro@gmail.com>
2024-08-12 15:57:39 +0900

    修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_08_09_063735) do
+ActiveRecord::Schema[7.0].define(version: 2024_08_09_063728) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -60,11 +60,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_08_09_063735) do
   end
 
   create_table "prefectures", charset: "utf8", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "shipping_days", charset: "utf8", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
